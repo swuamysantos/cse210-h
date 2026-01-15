9c2c14312984dc96c62ca8d286f0259e08c87d96
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-01-15 10:32:33 -0400

    actualizations

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,19 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
-        job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._jobTitle = "Receptionist";
+        job1._company = "Cenusa";
+        job1._startYear = 2025;
+        job1._endYear = 2026;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        job2._jobTitle = "Stagiary";
+        job2._company = "Motorola";
+        job2._startYear = 2026;
+        job2._endYear = 2029;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Swuamy Santos";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
