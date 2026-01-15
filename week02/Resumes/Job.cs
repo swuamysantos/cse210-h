01a060565ba2281ed1a6a280891740ffbf135c51
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-01-15 10:22:03 -0400

    my activity

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,8 @@
+public class Job
+{
+    // Member variables
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Google";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Console.WriteLine(job1._company);
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Amazon";
+        job2._startYear = 2018;
+        job2._endYear = 2020;
+
+        Console.WriteLine(job2._company);
     }
 }
