934d5ef27704cb1f3fa0542fab82c838850c3d62
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-01-15 10:26:50 -0400

    avctualizations

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,18 +6,22 @@ class Program
     {
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Google";
-        job1._startYear = 2020;
-        job1._endYear = 2023;
-
-        Console.WriteLine(job1._company);
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
         Job job2 = new Job();
-        job2._jobTitle = "Web Developer";
-        job2._company = "Amazon";
-        job2._startYear = 2018;
-        job2._endYear = 2020;
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-        Console.WriteLine(job2._company);
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
