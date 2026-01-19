1e78ca0c63f601b2f945708c5a5202dec2b1106c
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-01-19 08:57:43 -0400

    my journal program i did in another pc and in another github now i needed to past here! Sorry

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter writer = new StreamWriter(filename))
+        {
+            foreach (Entry entry in _entries)
+            {
+                writer.WriteLine(entry.ToFileString());
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+
+        string[] lines = File.ReadAllLines(filename);
+        foreach (string line in lines)
+        {
+            _entries.Add(Entry.FromFileString(line));
+        }
+    }
+}
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        bool running = true;
+
+        while (running)
+        {
+            Console.WriteLine("Journal Menu:");
+            Console.WriteLine("1. Write a new entry");
+            Console.WriteLine("2. Display the journal");
+            Console.WriteLine("3. Save the journal to a file");
+            Console.WriteLine("4. Load the journal from a file");
+            Console.WriteLine("5. Quit");
+            Console.Write("Choose an option: ");
+
+            string choice = Console.ReadLine();
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case "1":
+                    string prompt = promptGenerator.GetRandomPrompt();
+                    Console.WriteLine(prompt);
+                    Console.Write("> ");
+                    string response = Console.ReadLine();
+                    string date = DateTime.Now.ToShortDateString();
+
+                    Entry entry = new Entry(date, prompt, response);
+                    journal.AddEntry(entry);
+                    break;
+
+                case "2":
+                    journal.DisplayAll();
+                    break;
+
+                case "3":
+                    Console.Write("Enter filename to save: ");
+                    string saveFile = Console.ReadLine();
+                    journal.SaveToFile(saveFile);
+                    Console.WriteLine("Journal saved.\n");
+                    break;
+
+                case "4":
+                    Console.Write("Enter filename to load: ");
+                    string loadFile = Console.ReadLine();
+                    journal.LoadFromFile(loadFile);
+                    Console.WriteLine("Journal loaded.\n");
+                    break;
+
+                case "5":
+                    running = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid option.\n");
+                    break;
+            }
+        }
     }
 }
