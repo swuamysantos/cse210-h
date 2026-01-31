c9d669a0752210d78352464cf46bde3ef7ee17a4
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-01-31 18:22:36 -0400

    fOUNDATIONS 2

---
+++ week04/OnlineOrdering/Address.cs
@@ -0,0 +1,27 @@
+public class Address
+{
+    private string _street;
+    private string _city;
+    private string _state;              
+    private string _country;
+
+    public Address(string street, string city, string state, string country)
+    {
+        _street = street;
+        _city = city;
+        _state = state;
+        _country = country;
+    }   
+
+    public bool IsInUSA()
+    {
+        return _country.ToUpper() == "USA" || _country.ToUpper() == "UNITED STATES" || _country.ToUpper() == "UNITED STATES OF AMERICA";
+    }
+    public string GetFullAddress()
+    {
+        return $"{_street},\n{_city},\n{_state},\n{_country}";
+    }
+
+}
+
+
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("1912 Gurupi", "Amazonas", "AM", "Brasil");
+        Customer customer1 = new Customer("Swuamy Santos", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Keyboard", "P111", 30, 1));
+        order1.AddProduct(new Product("Mouse", "P222", 20, 2));
+
+        Address address2 = new Address("456 Maple Bear Ave", "Toronto", "ON", "Canada");
+        Customer customer2 = new Customer("Ana Silva", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Headphones", "P333", 50, 1));
+        order2.AddProduct(new Product("Webcam", "P444", 70, 1));
+
+        DisplayOrder(order1);
+        Console.WriteLine();
+        DisplayOrder(order2);
+    }
+
+    static void DisplayOrder(Order order)
+    {
+        Console.WriteLine(order.GetPackingLabel());
+        Console.WriteLine(order.GetShippingLabel());
+        Console.WriteLine($"Total Price: ${order.GetTotalPrice()}");
     }
 }
