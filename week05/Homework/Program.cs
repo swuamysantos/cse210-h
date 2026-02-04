73cf05e72c3533bd0fe22d06dde63884195ecedf
swuamysantos <contasomenteparameusjogos@gmail.com>
2026-02-04 08:49:09 -0400

    exercise math and writing

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,27 @@
-using System;
-
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment simpleAssignment = new Assignment("Swuamy Santos", "Multiplication");
+        Console.WriteLine(simpleAssignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment(
+            "Jhefferson Parrag",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writing = new WritingAssignment(
+            "Alessandra Lima",
+            "Brasilians History",
+            "The Way of Colonizations"
+        );
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
